7cab5f3ca5865aa0d41bab331f0aff411a51e675
Katlyn Williams <kwilliams52@wsutech.edu>
2022-03-17 22:28:28 -0500

    CAC 102 Exercise 14 Testing Code Fixed

--- Exercises/exercise14.py
+++ Exercises/exercise14.py
@@ -10,49 +10,57 @@ import unittest
 # the function in a module called city_functions.py Create a file called test_cities.py
 # that tests the function you just wrote with unittest. Write a method called
 # test_city_country() to verify that calling with the appropriate values runs correctly.
-def City_Country(city_name, country_name):
+def city_country(city_name, country_name):
     return "{0}, {1}".format(city_name, country_name)
-
-    def test_city_country(self):
-        formatted = City_Country("New York", "The United States")
-        self.assertEqual(formatted, "New York The United States")
-
-
-City_Country("New York", "The United States")
 
 
 # 2) 11 - 2
 # Modify the above to have a third parameter, population.
-def City_Country(city_name, country_name, population=""):
-    return "{0}, {1} population {2}".format(city_name, country_name, population)
+def city_country2(city_name, country_name, population=" "):
+    return "{0}, {1} - population {2}".format(city_name, country_name, population)
 
-    def test_city_country(self):
-        formatted = City_Country("New York", "The United States", 50000)
-        self.assertEqual(formatted, "New York The United States population 50000")
 
 # 3) 11 - 3 Employee
 # Write a class called Employee. THe __init__() method should take first_name,
 # last_name, annual_salary and store then as attributes. Write a method called
 # give_raise() that adds 5000 to annual salary by default but accepts other values.
 # Test the class.
-class Employee():
+class Employee:
     def __init__(self, first_name, last_name, annual_salary):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.annual_salary = annual_salary
+        self._first_name = first_name
+        self._last_name = last_name
+        self._annual_salary = annual_salary
+
+    def __str__(self):
+        return f"{self._first_name} {self._last_name} {self._annual_salary}"
 
     def give_raise(self, amount=5000):
-        self.annual_salary += amount
+        self._annual_salary = str(int(self._annual_salary) + amount)
+        return f"{self._first_name} {self._last_name} {self._annual_salary}"
+
+
+class TestingExercises(unittest.TestCase):
+    def test_city_country(self):
+        formatted_city = city_country("New York", "The United States")
+        self.assertEqual(formatted_city, "New York, The United States")
+
+    def test_city_country2(self):
+        formatted_city_2 = city_country("New York", "The United States - population 50000")
+        self.assertEqual(formatted_city_2, "New York, The United States - population 50000")
 
     def setUp(self):
-        formatted = Employee("Jane", "Foster", 500000)
-        def test_give_default_raise(self):
-            def give_raise():
-                self.assertEqual(formatted, "Jane Foster 505000")
-        def test_give_custom_raise(self):
-            def give_raise(amount = 10000):
-                self.assertEqual(formatted, "Jane Foster 510000")
+        self. my_employee = Employee("Jane", "Foster", 500000)
+
+    def test_give_default_raise(self):
+        self.my_employee.give_raise()
+        self.employee1 = self.my_employee.__str__()
+        self.assertEqual(self.employee1, "Jane Foster 505000")
+
+    def test_give_custom_raise(self):
+        self.my_employee.give_raise(10000)
+        self.employee1 = self.my_employee.__str__()
+        self.assertEqual(self.employee1, "Jane Foster 510000")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
